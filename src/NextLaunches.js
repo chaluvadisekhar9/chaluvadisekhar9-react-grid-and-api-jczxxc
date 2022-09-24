125a997ee46175a565f2cad9a871ff2472b5ecf7
Chandu <53987636+chaluvadisekhar9@users.noreply.github.com>
2022-09-24 20:17:04 +0530

    BalaChandra second commit

--- src/NextLaunches.js
+++ src/NextLaunches.js
@@ -3,14 +3,26 @@ import React, { Component } from 'react'
 export default class NextLaunches extends Component {
 constructor(props){
     super(props);
-    this.state = []
+    this.state = {
+        newLanunches : []
+    }
 }
 componentDidMount(){
-    fetch("")
+   const fetchData =  fetch("https://api.spacexdata.com/v4/launches/upcoming").then((response)=>response.json())
+   .then((newLanunches)=>
+   {
+    this.setState({newLanunches:newLanunches})
+   }
+   )
+   .catch((err)=>console.log(err))
 }
   render() {
     return (
-      <div className='nextLaunches'>NextLaunches</div>
+      <div className='nextLaunches'>
+        {this.state.newLanunches.map((item,id)=>{
+            <li>{item.id}</li>
+        })}
+      </div>
     )
   }
 }
